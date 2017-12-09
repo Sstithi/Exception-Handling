200faa06713627ca9e2ceaf85af0b14dac755da1
Sstithi <showmitasaha13313@gmail.com>
2017-12-10 01:54:04 +0600

    Throws

--- src/Xception3.java
+++ src/Xception3.java
@@ -2,21 +2,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Xception3 {
-	public static void main(String[] args) {
+public class Xception3  {
+	public static void main(String[] args) throws Exception{
 		File f = new File("DemoFile.txt");
 		Scanner sc ;
-		String input ="abcd";
-		try {
+		String input ="";
+		
 			 sc  = new Scanner(f);
 			 input = sc.nextLine();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-		System.out.println("File not found");
-		}
 		
-		System.out.println(input);
+			
+	System.out.println(input);
+		
 	}
-	
 
 }
