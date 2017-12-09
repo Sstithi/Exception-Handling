a1fbbf3ce1eb5d9dd3dca39e51255a077b0f211e
Sstithi <showmitasaha13313@gmail.com>
2017-12-10 02:09:41 +0600

    Customized Exception

--- src/Xception3.java
+++ src/Xception3.java
@@ -1,19 +1,39 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+//import java.io.File;
+//import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Xception3  {
-	public static void main(String[] args) throws Exception{
-		File f = new File("DemoFile.txt");
-		Scanner sc ;
-		String input ="";
+	public static void main(String[] args)
+	{
 		
-			 sc  = new Scanner(f);
-			 input = sc.nextLine();
-		
-			
-	System.out.println(input);
+	int number = inputInt("Enter the number: " );
+	try {
+		if (number>10)
+		{
+			//Exception e = new Exception();
+			throw new MyException();
+		}
 		
 	}
+	catch (MyException e)
+	{
+		System.out.println("Number can't be greater than 12");
+	}
+	System.out.println("Here ends the program");
+	}
 
+	public static int inputInt(String m) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		System.out.println(m);
+		return sc.nextInt();
+	}
+
+}
+class MyException extends Exception
+{
+	public MyException ()
+	{
+		System.out.println("Number can't be greater then 10");
+	}
 }
